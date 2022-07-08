1015f029af4e68e9da03c3dca03f27824a24025e
foxjazz <fox21@foxjazz.net>
2022-07-08 11:13:39 -0600

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DatePickerSampleComponent } from './date-picker-sample/date-picker-sample.component';
 import {MatDatepickerModule} from '@angular/material/datepicker'
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatCommonModule,  MatNativeDateModule} from '@angular/material/core'
+import {MatInputModule} from "@angular/material/input";
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,10 +17,14 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     BrowserModule,
     BrowserAnimationsModule,
     MatDatepickerModule,
+    MatNativeDateModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCommonModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/date-picker-sample/date-picker-sample.component.ts
+++ src/app/date-picker-sample/date-picker-sample.component.ts
@@ -3,6 +3,6 @@ import {Component} from '@angular/core';
 /** @title Basic datepicker */
 @Component({
   selector: 'date-picker-sample',
-  templateUrl: 'date-picker-sample.html',
+  templateUrl: 'date-picker-sample.component.html',
 })
 export class DatePickerSampleComponent {}
